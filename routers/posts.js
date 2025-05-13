a4af4156c0eebeb080023843fe7d9b2b49c0edd6
dannyarena <arenadanny71@gmail.com>
2025-05-13 19:43:35 +0200

    Add GET route for post details by ID in posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -5,4 +5,10 @@ const { posts } = require('../data/posts');
 router.get('/', (req, res) => {
     res.send('Lista dei post');
 });
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Dettagli del post ${id}`);
+});
+
 module.exports = router;
