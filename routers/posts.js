df373f283f52d8c520bd3c30a21b6f33d8d3ad08
dannyarena <arenadanny71@gmail.com>
2025-05-13 19:29:08 +0200

    Remove package.json from routers and update posts.js to include a GET route for posts

--- routers/posts.js
+++ routers/posts.js
@@ -1,5 +1,8 @@
 const express = require ('express');
 const router = express.Router();
-const posts = require (' ../data/posts.js');
+const { posts } = require('../data/posts');
 
-module.exports = router
+router.get('/', (req, res) => {
+    res.send('Lista dei post');
+});
+module.exports = router;
